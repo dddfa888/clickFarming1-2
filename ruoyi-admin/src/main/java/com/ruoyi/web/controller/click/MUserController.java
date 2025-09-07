bbbba8450e8a686583366d554c35c89f026576fa
ansj3 <aaa@gmail.com>
2025-09-07 15:44:42 +0800

    返现手动推送

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/click/MUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/click/MUserController.java
@@ -11,6 +11,8 @@ import javax.validation.constraints.NotNull;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.business.domain.MRewardRecord;
 import com.ruoyi.business.service.IMRewardRecordService;
+import com.ruoyi.click.domain.MMoneyInvestWithdraw;
+import com.ruoyi.click.mapper.MMoneyInvestWithdrawMapper;
 import com.ruoyi.click.mapper.MNotifyMapper;
 import com.ruoyi.click.service.IMMoneyInvestWithdrawService;
 import com.ruoyi.common.annotation.FrontAccess;
@@ -63,6 +65,9 @@ public class MUserController extends BaseController {
     @Autowired
     private MNotifyMapper mNotifyMapper;
 
+    @Autowired
+    private MMoneyInvestWithdrawMapper mMoneyInvestWithdrawMapper;
+
 
     @GetMapping("/userInfo")
     @FrontAccess
@@ -78,12 +83,11 @@ public class MUserController extends BaseController {
 
     /**
      * 修改用户余额，前端输入增减的变化量，后端计算出余额新值
-     *
      * @param balanceModel
      * @return
      */
     @PostMapping("changeBalance")
-    public AjaxResult changeBalance(HttpServletRequest request,@Validated @RequestBody BalanceModel balanceModel) {
+    public AjaxResult changeBalance(HttpServletRequest request, @Validated @RequestBody BalanceModel balanceModel) {
         MUser mUser = mUserService.selectMUserByUid(balanceModel.getUid());
         BigDecimal accountForward = mUser.getAccountBalance();
         String userName = tokenService.getLoginUser(request).getUser().getUserName();
@@ -91,6 +95,7 @@ public class MUserController extends BaseController {
         HashMap<String, Object> map = mUserService.updateBalance(mUser, balanceModel);
 
         // 日志记录
+        Integer type1 = balanceModel.getType1().intValue(); // 获取type1值
         BigDecimal accountBack = (BigDecimal) map.get("accountBalance");
         Integer type = (Integer) map.get("type");
         MAccountChangeRecords changeRecords = new MAccountChangeRecords();
@@ -99,48 +104,80 @@ public class MUserController extends BaseController {
         changeRecords.setAccountForward(accountForward);
         changeRecords.setAccountBack(accountBack);
         changeRecords.setUid(String.valueOf(balanceModel.getUid()));
-        changeRecords.setDescription(userName+"[后台增减余额] "+balanceModel.getReason());
+        //changeRecords.setDescription(userName+"[后台增减余额] "+balanceModel.getReason());
         changeRecords.setTransactionType(1);
         accountChangeRecordsService.insertMAccountChangeRecords(changeRecords);
-        //扣减余额
-        if(balanceModel.getBalance().signum()<0){
-            int read = 0;
-            //金额格式转换
-            BigDecimal formattedAmount = balanceModel.getBalance().abs()
-                    .setScale(2, RoundingMode.DOWN);
-            //后台扣减余额
-            String title = "Trừ số dư ở phần mềm quản trị";
-            //内容
-            String content = "Hệ thống đã thanh toán" + formattedAmount + "$ cho bạn!";
-            //新增提现消息
-            mNotifyMapper.insertNotify(mUser.getUid(),mUser.getLoginAccount(),title,content,read);
-        }
 
-        //如果是增加余额，添加奖励记录
-        if(balanceModel.getBalance().signum()>0){
-            MRewardRecord mRewardRecord= new MRewardRecord();
-            mRewardRecord.setUserId(mUser.getUid());
-            mRewardRecord.setUserName(mUser.getLoginAccount());
-            mRewardRecord.setRewardTime(DateUtils.getNowDate());
-            mRewardRecord.setRewardAmount(balanceModel.getBalance());
-            mRewardRecord.setBalanceBefore(accountForward);
-            mRewardRecord.setBalanceAfter(accountBack);
-            mRewardRecord.setDescription(balanceModel.getReason());
-            mRewardRecord.setCreateTime(mRewardRecord.getRewardTime());
-            mRewardRecordService.insertMRewardRecord(mRewardRecord);
-            //新增取款记录保存信息
-            int read = 0;
-            //金额格式转换
-            BigDecimal formattedAmount = balanceModel.getBalance().abs()
-                    .setScale(2, RoundingMode.DOWN);
-            //后台新增余额
-            String title = "Thêm số dư ở phần mềm quản trị";
-            //内容消息
-            String content = "Bạn đã nạp"+ formattedAmount + "$ thành công!";
-            //新增提现消息
-            mNotifyMapper.insertNotify(mUser.getUid(),mUser.getLoginAccount(),title,content,read);
-        }
+        // 处理 type1 = 1 的情况：充值或提现，插入 m_money_invest_withdraw 表
+        if (type1 == 1) {
+            MMoneyInvestWithdraw investWithdraw = new MMoneyInvestWithdraw();
+            investWithdraw.setUserId(balanceModel.getUid());
+            investWithdraw.setUserName(mUser.getLoginAccount());
+            //investWithdraw.setUserType(mUser.getUserType());
+            investWithdraw.setAmount(balanceModel.getBalance().abs());
+            investWithdraw.setType(balanceModel.getBalance().signum() > 0 ? "1" : "0");
+            investWithdraw.setStatus(1); // 充值中状态为1，提现中状态为0（可根据实际需求调整）
+            investWithdraw.setCreateTime(new Date());
+            investWithdraw.setUpdateTime(new Date());
+            investWithdraw.setIsRead(0); // 假设未读为0
+            mMoneyInvestWithdrawMapper.insert(investWithdraw);
 
+            // 原有扣减余额逻辑
+            if(balanceModel.getBalance().signum()<0){
+                int read = 0;
+                //金额格式转换
+                BigDecimal formattedAmount = balanceModel.getBalance().abs()
+                        .setScale(2, RoundingMode.DOWN);
+                //后台扣减余额
+                String title = "Trừ số dư ở phần mềm quản trị";
+                //内容
+                String content = "Hệ thống đã thanh toán" + formattedAmount + "$ cho bạn!";
+                //新增提现消息
+                mNotifyMapper.insertNotify(mUser.getUid(),mUser.getLoginAccount(),title,content,read);
+            }
+
+            if(balanceModel.getBalance().signum()>0) {
+                //新增取款记录保存信息
+                int read = 0;
+                //金额格式转换
+                BigDecimal formattedAmount = balanceModel.getBalance().abs()
+                        .setScale(2, RoundingMode.DOWN);
+                //后台新增余额
+                String title = "Thêm số dư ở phần mềm quản trị";
+                //内容消息
+                String content = "Bạn đã nạp"+ formattedAmount + "$ thành công!";
+                //新增提现消息
+                mNotifyMapper.insertNotify(mUser.getUid(),mUser.getLoginAccount(),title,content,read);
+            }
+        }
+        // 处理 type1 = 2 的情况：奖励，插入 m_account_change_records 表
+        else if (type1 == 2) {
+            // 原有增加余额逻辑
+            if(balanceModel.getBalance().signum()>0){
+                MRewardRecord mRewardRecord= new MRewardRecord();
+                mRewardRecord.setUserId(mUser.getUid());
+                mRewardRecord.setUserName(mUser.getLoginAccount());
+                mRewardRecord.setRewardTime(DateUtils.getNowDate());
+                mRewardRecord.setRewardAmount(balanceModel.getBalance());
+                mRewardRecord.setBalanceBefore(accountForward);
+                mRewardRecord.setBalanceAfter(accountBack);
+                //mRewardRecord.setDescription(balanceModel.getReason());
+                mRewardRecord.setCreateTime(mRewardRecord.getRewardTime());
+                mRewardRecordService.insertMRewardRecord(mRewardRecord);
+
+                //新增取款记录保存信息
+                int read = 0;
+                //金额格式转换
+                BigDecimal formattedAmount = balanceModel.getBalance().abs()
+                        .setScale(2, RoundingMode.DOWN);
+                //后台新增余额
+                String title = "Trả lại chi phí nâng cấp";
+                //内容消息
+                String content = "QUÀ THƯỞNG"+ formattedAmount + "$";
+                //新增提现消息
+                mNotifyMapper.insertNotify(mUser.getUid(),mUser.getLoginAccount(),title,content,read);
+            }
+        }
         // 升级等级
         //mUserService.upgrade(mUser.getUid());
         return success();
